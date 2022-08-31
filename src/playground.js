3b71e3f7a84df5f980e7f60e438e410587de39e3
Riwalenn <7VdDQCwL3L&M6#q>
2022-08-31 15:16:52 +0200

    [MAJ] MainNav : add header

--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,15 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
-
-console.log([...fruits, ...vegetables]);
+const developer = {
+    salary: 100000,
+    experience: 4.5,
+    techStack: ["Vue", "HTML", "CSS"],
+    lookingForWork: true,
+    doubleSalary() {
+        this.salary = this.salary * 2;
+        this.lookingForWork = false;
+    },
+};
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+developer.doubleSalary();
+console.log(developer.salary);
+console.log(developer.lookingForWork);
