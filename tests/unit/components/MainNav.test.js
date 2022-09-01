76d26051d1248115ccdfa3aad13f1714e17d4c4d
Riwalenn <7VdDQCwL3L&M6#q>
2022-09-01 15:40:51 +0200

    [TEST] v-for directive

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,19 @@ describe("MainNav", () => {
         const wrapper = mount(MainNav);
         expect(wrapper.text()).toMatch("Cat Careers");
     });
+
+    it('displays menu items for navigation', function () {
+        const wrapper = mount(MainNav);
+        const navigationMenuItems = wrapper.findAll("li");
+        const navigationMenuTexts = navigationMenuItems.map(item => item.text());
+
+        expect(navigationMenuTexts).toEqual([
+            "Teams",
+            "Locations",
+            "Life at Cat Corp",
+            "How we hire",
+            "Students",
+            "Jobs",
+        ]);
+    });
 });
