2eb9b37adc07a7c48e3d213ea5ca272e54c465fb
Riwalenn <7VdDQCwL3L&M6#q>
2022-09-02 10:01:44 +0200

    [ADD] Profile image component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+      :src="imageLink" alt="image profil" class="w-8 h-8 object-contain rounded-3xl"
+  >
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink: "https://i.pinimg.com/originals/ab/5d/e0/ab5de0df53da6094b6b322360ca4d424.png",
+    };
+  },
+}
+</script>
