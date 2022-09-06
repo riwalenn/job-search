c599f33f80708afb8f9e72f672c163bb69bb6f52
RIWALENN BAS <riwalenn@gmail.com>
2022-09-06 23:00:38 +0200

    [MAJ] Changing the action verb in headline

---
+++ src/components/Headline.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h1>{{ action }} pour tous</h1>
+    <h2>Trouvez votre prochain emploi chez Career Corp.</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  data() {
+    return {
+      action: "Créé",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Créé", "Conçu", "Développé"];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1) % 3;
+        const nextAction = actions[nextActionIndex];
+        this.action = nextAction;
+      }, 3000);
+    },
+  },
+};
+</script>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,7 +6,7 @@
           1 colonne
         </div>
         <div class="col-start-2 col-span-5 border border-solid border-blue-700">
-          5 colonne
+          <headline />
         </div>
         <div class="col-start-7 col-span-5 border border-solid border-blue-700">
           5 colonne
@@ -22,19 +22,12 @@
 </template>
 
 <script>
+import Headline from "@/components/Headline";
+
 export default {
   name: "HeroSection",
-  beforeCreate() {
-    console.log("component is about to be created");
-  },
-  created() {
-    console.log("component has been created");
-  },
-  beforeMount() {
-    console.log("component is about to be mounted");
-  },
-  mounted() {
-    console.log("component has been mounted in the DOM");
+  components: {
+    Headline,
   },
 };
 </script>
