548ce640771b03d92b8ba83ce3312a76a55272cf
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-09 15:45:36 +0200

    [TEST] Event Emission

---
+++ tests/unit/components/Shared/Textinput.test.js
@@ -0,0 +1,20 @@
+import { mount } from "@vue/test-utils";
+import TextInput from "@/components/Shared/TextInput";
+
+describe("TextInput", () => {
+  it("communicates that user has entered character", function () {
+    const wrapper = mount(TextInput, {
+      props: {
+        modelValue: "",
+      },
+    });
+    const input = wrapper.find("input");
+
+    input.setValue("N");
+    input.setValue("NY");
+    input.setValue("NYC");
+
+    const messages = wrapper.emitted()["update:modelValue"];
+    expect(messages).toEqual([["N"], ["NY"], ["NYC"]]);
+  });
+});
