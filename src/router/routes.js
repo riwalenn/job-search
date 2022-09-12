7e7218a6855cd00cc8562e9e2872c5bc495381c5
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-12 14:48:17 +0200

    [ADD] page liste emplois

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,11 @@ const routes = [
     component: () => import("@/views/HomeView"),
   },
   {
+    path: "/jobs/results/:id",
+    name: "ListeEmplois",
+    component: () => import("@/views/JobView"),
+  },
+  {
     path: "/jobs/results",
     name: "Resultats",
     component: () => import("@/views/JobResultsView"),
---
+++ src/views/JobView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Emplois</div>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+};
+</script>
