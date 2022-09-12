668d837c5c1435cc242a0e7b5d705523fb98b8dd
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-12 14:01:37 +0200

    [MAJ] refactoring : v-bind menu

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -14,13 +14,16 @@
           <ul class="flex h-full p-0 m-0 list-none">
             <li
               v-for="menuItem in menuItems"
-              :key="menuItem"
+              :key="menuItem.text"
               class="h-full ml-9 first:ml-0"
               data-test="main-nav-list-item"
             >
-              <a href="" class="flex items-center h-full py-2.5">
-                {{ menuItem }}
-              </a>
+              <router-link
+                :to="menuItem.url"
+                class="flex items-center h-full py-2.5"
+              >
+                {{ menuItem.text }}
+              </router-link>
             </li>
           </ul>
         </nav>
@@ -53,11 +56,11 @@ export default {
   data() {
     return {
       menuItems: [
-        "Qui sommes-nous ?",
-        "Localisations",
-        "Nous rejoindre",
-        "Etudiants",
-        "Emplois",
+        { text: "Qui sommes-nous ?", url: "/" },
+        { text: "Localisations", url: "/" },
+        { text: "Nous rejoindre", url: "/" },
+        { text: "Etudiants", url: "/" },
+        { text: "Emplois", url: "/jobs/results" },
       ],
       isLoggedIn: false,
     };
