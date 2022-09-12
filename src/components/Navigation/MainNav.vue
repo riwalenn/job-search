23b6591f9915b9c949c9e0838ad0e7dd6362a3c2
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-12 12:16:00 +0200

    [MAJ] adding bind url to logo

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -4,9 +4,12 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a :href="url" class="flex items-center h-full text-xl">
-          {{ company }}
-        </a>
+        <router-link
+          :to="{ name: 'Accueil' }"
+          class="flex items-center h-full text-xl"
+        >
+          Careers
+        </router-link>
         <nav class="h-full ml-12">
           <ul class="flex h-full p-0 m-0 list-none">
             <li
@@ -50,7 +53,6 @@ export default {
   data() {
     return {
       company: "Careers",
-      url: "https://careers.riwalennbas.com",
       menuItems: [
         "Qui sommes-nous ?",
         "Localisations",
