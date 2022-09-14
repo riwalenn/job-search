00d3e03bdd57191072efc7938164ab6fc0fe246a
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-14 16:48:21 +0200

    [TEST] job listing

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -5,6 +5,8 @@ describe("JobListing", () => {
   const createJobProps = (jobProps = {}) => ({
     title: "Vue Developer",
     organization: "ILABS",
+    locations: ["Indianapolis"],
+    qualifications: ["Succeed"],
     ...jobProps,
   });
   const createConfig = (jobProps) => ({
@@ -23,12 +25,43 @@ describe("JobListing", () => {
   it("renders job title", function () {
     const jobProps = createJobProps({ title: "Vue Programmer" });
     const wrapper = mount(JobListing, createConfig(jobProps));
+
     expect(wrapper.text()).toMatch("Vue Programmer");
   });
 
   it("renders job organization", function () {
     const jobProps = createJobProps({ organization: "ILABS" });
     const wrapper = mount(JobListing, createConfig(jobProps));
+
     expect(wrapper.text()).toMatch("ILABS");
+  });
+
+  it("renders job locations", function () {
+    const jobProps = createJobProps({ locations: ["Orlando", "Jacksonville"] });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+
+    expect(wrapper.text()).toMatch("Orlando");
+    expect(wrapper.text()).toMatch("Jacksonville");
+  });
+
+  it("renders job qualifications", function () {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Code", "Develop"],
+    });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+
+    expect(wrapper.text()).toMatch("Code");
+    expect(wrapper.text()).toMatch("Develop");
+  });
+
+  it("links to individual job's page", function () {
+    const jobProps = createJobProps({
+      id: 15,
+    });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    const jobPageLink = wrapper.findComponent(RouterLinkStub);
+    const toProp = jobPageLink.props("to");
+
+    expect(toProp).toBe("/jobs/results/15");
   });
 });
