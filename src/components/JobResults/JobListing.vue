01dddfaedbeaad7b72003ed9850cd0067d1e0ca7
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-14 16:07:46 +0200

    [MAJ] render job's data

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,25 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-        <h2 class="mb-2 text-2xl">Développeur backend PHP</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>BLEBLEH COMPAGNIE</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>Toulouse, France</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -20,22 +28,17 @@
           <h3 class="mt-1 mb-2">Qualifications :</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bonne connaissance des design patterns liés à la POO</li>
-              <li>Maîtrise des outils de supervision et administration unix</li>
-              <li>Stack principale : PHP 7/8, Symfony 4/5</li>
-              <li>
-                Idéalement expérience : Go et/ou NodeJS et/ou Java
-                Spring/Quarkus
-              </li>
-              <li>
-                Cloud : AWS, RabbitMQ, AMQP, Docker, Terraform, Ansible - CI/CD
-                : Jenkins, Github, RobotFramework
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-brand-blue-1"
             >en voir +</router-link
           >
         </div>
@@ -47,5 +50,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
