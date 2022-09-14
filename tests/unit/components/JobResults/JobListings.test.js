bacaca68055d0b5fd0e91cfacb97377d6f44cb38
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-14 17:43:12 +0200

    [TEST] fixing failing jobListings Component tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -7,13 +7,35 @@ jest.mock("axios");
 describe("JobListings", () => {
   it("fetches jobs", function () {
     axios.get.mockReturnValue({ data: [] });
-    shallowMount(JobListings);
+    const $route = {
+      query: {
+        page: "5",
+      },
+    };
+    shallowMount(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job listing for a maximum of 10 jobs", async function () {
-    axios.get.mockReturnValue({ data: Array(10).fill({}) });
-    const wrapper = shallowMount(JobListings);
+    axios.get.mockReturnValue({ data: Array(15).fill({}) });
+    const $route = {
+      query: {
+        page: "1",
+      },
+    };
+    const wrapper = shallowMount(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
     expect(jobListings).toHaveLength(10);
