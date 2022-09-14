fcda8df3936d1aa51d179100ec5c7a0d4f0df066
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-14 17:57:27 +0200

    [MAJ] displaying page number at bottom of job listings

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -8,6 +8,11 @@
         data-test="job-listing"
       />
     </ol>
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-row flex-nowrap">
+        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -27,9 +32,12 @@ export default {
     };
   },
   computed: {
-    displayedJobs() {
+    currentPage() {
       const pageString = this.$route.query.page || "1";
-      const pageNumber = Number.parseInt(pageString);
+      return Number.parseInt(pageString);
+    },
+    displayedJobs() {
+      const pageNumber = this.currentPage;
       const firstJobIndex = (pageNumber - 1) * 10;
       const lastJobIndex = pageNumber * 10;
       return this.jobs.slice(firstJobIndex, lastJobIndex);
