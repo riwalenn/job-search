c1629dec73993aeb41545aa4a272b3938a90c7ba
RIWALENN BAS <riwalenn@gmail.com>
2022-09-17 23:04:21 +0200

    [MAJ] adding initial styles for JobFiltersSidebar Component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,12 +2,24 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    Filtres
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">Que voulez-vous faire ?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Effacer les filtres" type="secondary" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton";
+
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+  },
 };
 </script>
