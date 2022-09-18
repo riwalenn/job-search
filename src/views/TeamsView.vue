a2f5d8fda713f4ce7c43373e5f61520e4101193c
RIWALENN BAS <riwalenn@gmail.com>
2022-09-18 15:30:35 +0200

    [ADD] Named Slots

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,14 @@
 <template>
-  <header-container />
+  <header-container>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Equipe</h1>
+    </template>
+    <template #subtitle>
+      <h2 class="w-full my-4 text-base font-light">
+        It's awesome working here, why don't you come join us ?
+      </h2>
+    </template>
+  </header-container>
 </template>
 
 <script>
