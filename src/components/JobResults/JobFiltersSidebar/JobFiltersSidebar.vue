ca58643fc593e2096b56f619def0ed6bafab2604
RIWALENN BAS <riwalenn@gmail.com>
2022-09-18 11:10:34 +0200

    [ADD] Render Custom Content in Slot

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -9,7 +9,30 @@
           <action-button text="Effacer les filtres" type="secondary" />
         </div>
       </div>
-      <accordion> Hello, custom content </accordion>
+      <accordion>
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="w-1/2 h-8">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Between Vue and me" type="checkbox" class="mr-3" />
+                <label for="Between Vue and me">Between Vue</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+                <label for="Vue and a Half Men">Vue and a Half Men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </accordion>
     </section>
   </div>
 </template>
