3dde42b17383527806ad0314aefe9d075f929d48
RIWALENN BAS <riwalenn@gmail.com>
2022-09-18 14:33:43 +0200

    [TEST] Refactoring

--- tests/unit/components/Shared/Accordion.test.js
+++ tests/unit/components/Shared/Accordion.test.js
@@ -3,20 +3,27 @@ import { mount } from "@vue/test-utils";
 import Accordion from "@/components/Shared/Accordion";
 
 describe("Accordion", () => {
+  const createConfig = (config = {}) => ({
+    global: {
+      stubs: {
+        FontAwesomeIcon: true,
+      },
+    },
+    props: {
+      header: "Test Header",
+    },
+    slots: {
+      default: "<h3>My nested child</h3>",
+    },
+    ...config,
+  });
+
   it("renders child", async function () {
-    const wrapper = mount(Accordion, {
-      global: {
-        stubs: {
-          FontAwesomeIcon: true,
-        },
-      },
-      props: {
-        header: "Test Header",
-      },
-      slots: {
-        default: "<h3>My nested child</h3>",
-      },
-    });
+    const slots = {
+      default: "<h3>My nested child</h3>",
+    };
+    const config = { slots };
+    const wrapper = mount(Accordion, createConfig(config));
 
     expect(wrapper.text()).not.toMatch("My nested child");
     const clickableArea = wrapper.find("[data-test='clickable-area']");
@@ -27,16 +34,9 @@ describe("Accordion", () => {
 
   describe("when we do not provide custom child content", () => {
     it("renders default content", async function () {
-      const wrapper = mount(Accordion, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        props: {
-          header: "Test Header",
-        },
-      });
+      const slots = {};
+      const config = { slots };
+      const wrapper = mount(Accordion, createConfig(config));
 
       const clickableArea = wrapper.find("[data-test='clickable-area']");
 
