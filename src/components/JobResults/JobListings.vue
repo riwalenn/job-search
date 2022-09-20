336ff1deb6e61aa256239e778e033c47d0eaf040
Riwalenn Bas <riwalenn.bas@ilabs.fr>
2022-09-20 13:37:29 +0200

    [MAJ] Rendering Filtered Organization Jobs in JobListings Component

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -35,9 +35,9 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
-import { FETCH_JOBS } from "@/store";
+import { FETCH_JOBS, FILTERED_JOBS_BY_ORGANIZATIONS } from "@/store";
 import JobListing from "@/components/JobResults/JobListing";
 
 export default {
@@ -46,6 +46,7 @@ export default {
     JobListing,
   },
   computed: {
+    ...mapGetters([FILTERED_JOBS_BY_ORGANIZATIONS]),
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);
@@ -57,7 +58,9 @@ export default {
     },
     nextPage() {
       const nextPage = this.currentPage + 1;
-      const maxPage = Math.ceil(this.jobs.length / 10);
+      const maxPage = Math.ceil(
+        this.FILTERED_JOBS_BY_ORGANIZATIONS.length / 10
+      );
       return nextPage <= maxPage ? nextPage : undefined;
     },
     totalPage() {
@@ -67,9 +70,11 @@ export default {
       const pageNumber = this.currentPage;
       const firstJobIndex = (pageNumber - 1) * 10;
       const lastJobIndex = pageNumber * 10;
-      return this.jobs.slice(firstJobIndex, lastJobIndex);
+      return this.FILTERED_JOBS_BY_ORGANIZATIONS.slice(
+        firstJobIndex,
+        lastJobIndex
+      );
     },
-    ...mapState(["jobs"]),
   },
   async mounted() {
     this.FETCH_JOBS();
