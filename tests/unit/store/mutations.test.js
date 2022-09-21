1583267dcc0f2ab7340e8c9d788ee7b05b7a6d4f
RIWALENN BAS <riwalenn@gmail.com>
2022-09-21 14:31:33 +0200

    [TEST] adding mutations for Job Types

--- tests/unit/store/mutations.test.js
+++ tests/unit/store/mutations.test.js
@@ -1,4 +1,4 @@
-import { mutations } from "@/store/mutations";
+import mutations from "@/store/mutations";
 
 describe("mutations", () => {
   describe("LOGIN_USER", () => {
@@ -25,6 +25,15 @@ describe("mutations", () => {
 
       mutations.ADD_SELECTED_ORGANIZATIONS(state, ["Org1", "Org2"]);
       expect(state).toEqual({ selectedOrganizations: ["Org1", "Org2"] });
+    });
+  });
+
+  describe("ADD_SELECTED_JOB_TYPES", () => {
+    it("updates job types that the user has chosen to filter jobs by", () => {
+      const state = { selectedJobTypes: [] };
+
+      mutations.ADD_SELECTED_JOB_TYPES(state, ["Full-time", "Part-time"]);
+      expect(state).toEqual({ selectedJobTypes: ["Full-time", "Part-time"] });
     });
   });
 });
