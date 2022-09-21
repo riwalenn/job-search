1c114e6c0a32a9928a45b643c714f331278b7bb5
RIWALENN BAS <riwalenn@gmail.com>
2022-09-21 22:02:59 +0200

    [TEST] adding test in JobFilterSidebarOrganizations

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -54,6 +54,7 @@ export default {
     ...mapMutations([ADD_SELECTED_ORGANIZATIONS]),
     selectOrganization() {
       this.ADD_SELECTED_ORGANIZATIONS(this.selectedOrganizations);
+      this.$router.push({ name: "Resultats" });
     },
   },
 };
