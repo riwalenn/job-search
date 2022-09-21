0149f5e1ce08202a55eba14ea1329e1ba98db738
RIWALENN BAS <riwalenn@gmail.com>
2022-09-21 19:40:28 +0200

    [ADD] Getters with arguments

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,12 +4,15 @@ import {
   UNIQUE_JOB_TYPES,
   FILTERED_JOBS_BY_JOB_TYPES,
   FILTERED_JOBS,
+  INCLUDE_JOB_BY_ORGANIZATION,
+  INCLUDE_JOB_BY_JOB_TYPE,
 } from "@/store/constants";
 
 const getters = {
   [UNIQUE_ORGANIZATIONS](state) {
     const uniqueOrganizations = new Set();
     state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
+
     return uniqueOrganizations;
   },
   [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
@@ -22,6 +25,7 @@ const getters = {
   [UNIQUE_JOB_TYPES](state) {
     const uniqueJobTypes = new Set();
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
+
     return uniqueJobTypes;
   },
   [FILTERED_JOBS_BY_JOB_TYPES](state) {
@@ -31,6 +35,16 @@ const getters = {
       state.selectedJobTypes.includes(job.jobType)
     );
   },
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+    if (state.selectedOrganizations.length === 0) return true;
+
+    return state.selectedOrganizations.includes(job.organization);
+  },
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+    if (state.selectedJobTypes.length === 0) return true;
+
+    return state.selectedJobTypes.includes(job.jobType);
+  },
   [FILTERED_JOBS](state) {
     const noSelectedOrganizations = state.selectedOrganizations.length === 0;
     const noSelectedJobTypes = state.selectedJobTypes.length === 0;
@@ -38,10 +52,12 @@ const getters = {
     return state.jobs
       .filter((job) => {
         if (noSelectedOrganizations) return true;
+
         return state.selectedOrganizations.includes(job.organization);
       })
       .filter((job) => {
         if (noSelectedJobTypes) return true;
+
         return state.selectedJobTypes.includes(job.jobType);
       });
   },
