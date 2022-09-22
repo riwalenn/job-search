02870af914c85f29a63e84a20214573ddeb3d11e
RIWALENN BAS <riwalenn@gmail.com>
2022-09-22 18:51:20 +0200

    [MAJ] playground : multiple levels of reactivity

--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,5 @@
 const { ref, reactive, computed } = require("vue");
-console.log("intro to computed : ");
+console.log("--- intro to computed : ");
 let a = ref(1);
 let b = ref(2);
 
@@ -8,14 +8,14 @@ console.log(c.value);
 a.value = 10;
 console.log(c.value);
 
-console.log("computed function : ");
+console.log("--- computed function : ");
 let name = ref("Boris");
 let title = computed(() => name.value + " the Great");
 console.log(title.value);
 name.value = "Peter";
 console.log(title.value);
 
-console.log("computed with object : ");
+console.log("--- computed with object : ");
 const person = ref({
   name: "Boris",
 });
@@ -26,7 +26,7 @@ console.log(title2.value);
 person.value.name = "Peter";
 console.log(title2.value);
 
-console.log("reactive function :");
+console.log("--- reactive function :");
 const person2 = reactive({
   name: "Boris",
 });
@@ -37,13 +37,16 @@ console.log(title3.value);
 person2.name = "Peter";
 console.log(title3.value);
 
-console.log("multiple properties on reactive object :");
+console.log("--- multiple properties on reactive object :");
 const user = reactive({
   firstname: "Riwalenn",
   lastname: "Bas",
 });
 const titre = computed(() => `${user.firstname} ${user.lastname} the Great`);
+const titleLength = computed(() => titre.value.length);
 console.log(titre.value);
+console.log(titleLength.value);
 
 user.firstname = "Peter";
 console.log(titre.value);
+console.log(titleLength.value);
