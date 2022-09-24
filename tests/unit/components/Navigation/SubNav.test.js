c10533fef0cccbf76cd311c9622468ab434e8ca2
RIWALENN BAS <riwalenn@gmail.com>
2022-09-24 12:16:24 +0200

    [TEST] fixing failing test for SubNav

--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -1,16 +1,14 @@
 import { mount } from "@vue/test-utils";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+jest.mock("vuex");
+jest.mock("vue-router");
 
 import Subnav from "@/components/Navigation/Subnav";
 
 describe("Subnav", () => {
-  const createConfig = (routeName, $store = {}) => ({
+  const createConfig = () => ({
     global: {
-      mocks: {
-        $route: {
-          name: routeName,
-        },
-        $store,
-      },
       stubs: {
         FontAwesomeIcon: true,
       },
@@ -19,13 +17,15 @@ describe("Subnav", () => {
 
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const routeName = "Resultats";
-      const $store = {
+      useRoute.mockReturnValue({
+        name: "Resultats",
+      });
+      useStore.mockReturnValue({
         getters: {
           FILTERED_JOBS: [{ id: 1 }, { id: 2 }],
         },
-      };
-      const wrapper = mount(Subnav, createConfig(routeName, $store));
+      });
+      const wrapper = mount(Subnav, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.text()).toMatch("2 emplois");
     });
@@ -33,8 +33,15 @@ describe("Subnav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
-      const routeName = "Home";
-      const wrapper = mount(Subnav, createConfig(routeName));
+      useRoute.mockReturnValue({
+        name: "Home",
+      });
+      useStore.mockReturnValue({
+        getters: {
+          FILTERED_JOBS: [{ id: 1 }, { id: 2 }],
+        },
+      });
+      const wrapper = mount(Subnav, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
