35d5748fe7df8fa53423dd7b55aeb554bba699bf
RIWALENN BAS <riwalenn@gmail.com>
2022-09-24 17:06:54 +0200

    [MAJ] adding more Composables for Getters

--- tests/unit/store/composables.test.js
+++ tests/unit/store/composables.test.js
@@ -1,7 +1,11 @@
 import { useStore } from "vuex";
 jest.mock("vuex");
 
-import { useFilteredJobs } from "@/store/composables";
+import {
+  useFilteredJobs,
+  useUniqueJobTypes,
+  useUniqueOrganizations,
+} from "@/store/composables";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -15,6 +19,32 @@ describe("composables", () => {
       const result = useFilteredJobs();
 
       expect(result.value).toEqual([{ id: 1 }]);
+    });
+  });
+
+  describe("useUniqueJobTypes", () => {
+    it("retrieves unique job types from store", function () {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+        },
+      });
+      const result = useUniqueJobTypes();
+
+      expect(result.value).toEqual(new Set(["Full-time"]));
+    });
+  });
+
+  describe("useUniqueOrganizations", () => {
+    it("retrieves unique job organizations from store", function () {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Apple"]),
+        },
+      });
+      const result = useUniqueOrganizations();
+
+      expect(result.value).toEqual(new Set(["Apple"]));
     });
   });
 });
