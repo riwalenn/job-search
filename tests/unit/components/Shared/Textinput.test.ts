ce24285bc98deffab304c239d6a9acf1dc0dfdff
RIWALENN BAS <riwalenn@gmail.com>
2022-09-25 21:09:20 +0200

    [MAJ] Annotating Event Handlers

--- tests/unit/components/Shared/Textinput.test.ts
+++ tests/unit/components/Shared/Textinput.test.ts
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 
-import TextInput from "@/components/Shared/TextInput";
+import TextInput from "@/components/Shared/TextInput.vue";
 
 describe("TextInput", () => {
   it("communicates that user has entered character", () => {
