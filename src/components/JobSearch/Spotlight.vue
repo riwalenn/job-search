97a20b61a341a96b3e14ef6ab7550fe6869034b8
RIWALENN BAS <riwalenn@gmail.com>
2022-09-25 21:32:21 +0200

    [MAJ] Annotating Axios requests

--- src/components/JobSearch/Spotlight.vue
+++ src/components/JobSearch/Spotlight.vue
@@ -10,23 +10,31 @@
   </ul>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import { ref, onMounted } from "vue";
 import axios from "axios";
 
-export default {
+interface Spotlight {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+export default defineComponent({
   name: "Spotlight",
   setup() {
-    const spotlights = ref([]);
+    const spotlights = ref<Spotlight[]>([]);
     const getSpotlights = async () => {
       const baseUrl = process.env.VUE_APP_API_URL;
       const url = `${baseUrl}/spotlights`;
-      const response = await axios.get(url);
+      const response = await axios.get<Spotlight[]>(url);
       spotlights.value = response.data;
     };
     onMounted(getSpotlights);
 
     return { spotlights };
   },
-};
+});
 </script>
