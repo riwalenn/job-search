3b5ba305e278e415a9e60a5186f2832237aa12e0
RIWALENN BAS <riwalenn@gmail.com>
2022-09-25 18:26:47 +0200

    [MAJ] adding Type Annotations to Headline Component

--- src/components/JobSearch/Headline.vue
+++ src/components/JobSearch/Headline.vue
@@ -18,19 +18,29 @@
   </section>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import nextElementInList from "@/utils/nextElementInList";
 
-export default {
+interface Data {
+  action: string;
+  interval?: number;
+}
+
+interface ActionClasses {
+  [x: string]: boolean;
+}
+
+export default defineComponent({
   name: "Headline",
-  data() {
+  data(): Data {
     return {
       action: "Build",
-      interval: null,
+      interval: undefined,
     };
   },
   computed: {
-    actionClasses() {
+    actionClasses(): ActionClasses {
       return {
         [this.action.toLowerCase()]: true,
       };
@@ -50,7 +60,7 @@ export default {
       }, 3000);
     },
   },
-};
+});
 </script>
 
 <style scoped>
