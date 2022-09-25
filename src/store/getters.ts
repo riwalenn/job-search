41f84d9d0dc19729baab6c5c9e42cc6808eb5a76
RIWALENN BAS <riwalenn@gmail.com>
2022-09-25 16:08:21 +0200

    [MAJ] adding types to getters file

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -6,26 +6,29 @@ import {
   FILTERED_JOBS,
 } from "@/store/constants";
 
+import { Job } from "@/api/types";
+import { GlobalState } from "@/store/types";
+
 const getters = {
-  [UNIQUE_ORGANIZATIONS](state) {
-    const uniqueOrganizations = new Set();
+  [UNIQUE_ORGANIZATIONS](state: GlobalState) {
+    const uniqueOrganizations = new Set<string>();
     state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
     return uniqueOrganizations;
   },
-  [UNIQUE_JOB_TYPES](state) {
-    const uniqueJobTypes = new Set();
+  [UNIQUE_JOB_TYPES](state: GlobalState) {
+    const uniqueJobTypes = new Set<string>();
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
     return uniqueJobTypes;
   },
-  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state: GlobalState) => (job: Job) => {
     if (state.selectedOrganizations.length === 0) return true;
     return state.selectedOrganizations.includes(job.organization);
   },
-  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state: GlobalState) => (job: Job) => {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
-  [FILTERED_JOBS](state, getters) {
+  [FILTERED_JOBS](state: GlobalState, getters) {
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
       .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
