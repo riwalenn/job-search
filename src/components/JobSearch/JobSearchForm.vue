f6550f7fa8f5154a8a30f4ceeaaba2e7a484fc35
RIWALENN BAS <riwalenn@gmail.com>
2022-09-26 18:36:05 +0200

    [MAJ] adding Type Annotations to JobSearchForm

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -35,14 +35,14 @@
   </form>
 </template>
 
-<script>
-import { ref } from "vue";
+<script lang="ts">
+import { ref, defineComponent } from "vue";
 import { useRouter } from "vue-router";
 
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import TextInput from "@/components/Shared/TextInput.vue";
 
-export default {
+export default defineComponent({
   name: "JobSearchForm",
   components: {
     ActionButton,
@@ -62,5 +62,5 @@ export default {
 
     return { profession, lieu, searchForJobs };
   },
-};
+});
 </script>
