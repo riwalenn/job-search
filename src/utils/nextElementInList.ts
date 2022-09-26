526331eecc819ceea281560f7c1c50edbe6f89e5
RIWALENN BAS <riwalenn@gmail.com>
2022-09-26 18:41:33 +0200

    [MAJ] adding Type for Utility functions

--- src/utils/nextElementInList.ts
+++ src/utils/nextElementInList.ts
@@ -1,4 +1,4 @@
-const nextElementInList = (list, value) => {
+const nextElementInList = <T>(list: T[], value: T): T => {
   const currentActionIndex = list.indexOf(value);
   const nextValueIndex = (currentActionIndex + 1) % list.length;
   return list[nextValueIndex];
