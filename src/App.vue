450eacda80045be4841ea39a70a5484432ad6e3f
RIWALENN BAS <riwalenn@gmail.com>
2022-09-26 18:44:17 +0200

    [MAJ] adding Type Annotations to Hero and App Components

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import MainNav from "@/components/Navigation/MainNav.vue";
 
-export default {
+export default defineComponent({
   name: "App",
   components: {
     MainNav,
   },
-};
+});
 </script>
--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -44,17 +44,18 @@
   </main>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import Headline from "@/components/JobSearch/Headline.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
 import Spotlight from "@/components/JobSearch/Spotlight.vue";
 
-export default {
+export default defineComponent({
   name: "Hero",
   components: {
     Headline,
     JobSearchForm,
     Spotlight,
   },
-};
+});
 </script>
