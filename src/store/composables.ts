963b1bb1831dd19677ee409dec956ef126702a7b
RIWALENN BAS <riwalenn@gmail.com>
2022-09-26 18:54:58 +0200

    [MAJ] udpdating Composables to use Store Key

--- src/store/composables.ts
+++ src/store/composables.ts
@@ -10,24 +10,26 @@ import {
 
 import { Job } from "@/api/types";
 
+import { key } from "@/store";
+
 /* GETTERS */
 export const useFilteredJobs = () => {
-  const store = useStore();
+  const store = useStore(key);
   return computed<Job[]>(() => store.getters[FILTERED_JOBS]);
 };
 
 export const useUniqueJobTypes = () => {
-  const store = useStore();
+  const store = useStore(key);
   return computed<Set<string>>(() => store.getters[UNIQUE_JOB_TYPES]);
 };
 
 export const useUniqueOrganizations = () => {
-  const store = useStore();
+  const store = useStore(key);
   return computed<Set<string>>(() => store.getters[UNIQUE_ORGANIZATIONS]);
 };
 
 /* ACTION */
 export const useFetchJobsDispatch = () => {
-  const store = useStore();
+  const store = useStore(key);
   store.dispatch(FETCH_JOBS);
 };
