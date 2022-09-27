9a77ee3902d4e034f6c19cf65e15c647c2a372f5
RIWALENN BAS <riwalenn@gmail.com>
2022-09-27 18:59:41 +0200

    [TEST] TDD for Actions: FETCH_DEGREES

--- tests/unit/store/actions.test.ts
+++ tests/unit/store/actions.test.ts
@@ -4,6 +4,10 @@ jest.mock("@/api/getJobs");
 
 const getJobsMock = getJobs as jest.Mock;
 
+import getDegrees from "@/api/getDegrees";
+jest.mock("@/api/getDegrees");
+const getDegreesMock = getDegrees as jest.Mock;
+
 describe("actions", () => {
   describe("FETCH_JOBS", () => {
     beforeEach(() => {
@@ -24,6 +28,34 @@ describe("actions", () => {
       await actions.FETCH_JOBS(context);
       expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
         { id: 1, title: "Software Developer" },
+      ]);
+    });
+  });
+
+  describe("FETCH_DEGREES", () => {
+    beforeEach(() => {
+      getDegreesMock.mockResolvedValue([
+        {
+          id: 1,
+          degree: "Bachelor's",
+        },
+      ]);
+    });
+
+    it("makes a request to fetch degrees", async () => {
+      const context = { commit: jest.fn() };
+
+      await actions.FETCH_DEGREES(context);
+      expect(getDegrees).toHaveBeenCalled();
+    });
+
+    it("sends message to save received degrees in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+
+      await actions.FETCH_DEGREES(context);
+      expect(commit).toHaveBeenCalledWith("RECEIVE_DEGREES", [
+        { id: 1, degree: "Bachelor's" },
       ]);
     });
   });
