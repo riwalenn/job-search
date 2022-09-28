ba9846bfd7e3289a2e495a092015d3b066332784
RIWALENN BAS <riwalenn@gmail.com>
2022-09-28 21:12:50 +0200

    [TEST] adding test for JobFiltersSidebarPrompt Component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -3,16 +3,7 @@
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base font-semibold">Que voulez-vous faire ?</h3>
-        <div class="flex items-center text-sm">
-          <action-button
-            text="Effacer les filtres"
-            type="secondary"
-            @click="clearUserJobFilterSelections"
-          />
-        </div>
-      </div>
+      <job-filters-sidebar-prompt />
 
       <accordion header="Diplômes">
         <job-filters-sidebar-degrees />
@@ -31,37 +22,22 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useStore } from "vuex";
-
-import { key } from "@/store";
 
 import Accordion from "@/components/Shared/Accordion.vue";
-import ActionButton from "@/components/Shared/ActionButton.vue";
 
 import JobFiltersSidebarDegrees from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue";
 import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue";
 import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
-
-import { CLEAR_USER_JOB_FILTER_SELECTIONS } from "@/store/constants";
+import JobFiltersSidebarPrompt from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarPrompt.vue";
 
 export default defineComponent({
   name: "JobFiltersSidebar",
   components: {
     Accordion,
-    ActionButton,
     JobFiltersSidebarDegrees,
     JobFiltersSidebarJobTypes,
     JobFiltersSidebarOrganizations,
-  },
-  setup() {
-    const store = useStore(key);
-    const clearUserJobFilterSelections = () => {
-      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
-    };
-
-    return {
-      clearUserJobFilterSelections,
-    };
+    JobFiltersSidebarPrompt,
   },
 });
 </script>
---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarPrompt.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-row justify-between">
+    <h3 class="my-4 text-base font-semibold">Que voulez-vous faire ?</h3>
+    <div class="flex items-center text-sm">
+      <action-button
+        text="Effacer les filtres"
+        type="secondary"
+        data-test="clear-user-job-filters"
+        @click="clearUserJobFilterSelections"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
+
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import { CLEAR_USER_JOB_FILTER_SELECTIONS } from "@/store/constants";
+
+export default defineComponent({
+  name: "JobFiltersSidebarPrompt",
+  components: {
+    ActionButton,
+  },
+  setup() {
+    const store = useStore(key);
+    const clearUserJobFilterSelections = () => {
+      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
+    };
+
+    return {
+      clearUserJobFilterSelections,
+    };
+  },
+});
+</script>
