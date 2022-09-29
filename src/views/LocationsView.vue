2022464bdb9a888721df5ad7f87f7bbf2f109903
RIWALENN BAS <riwalenn@gmail.com>
2022-09-29 21:47:32 +0200

    [ADD] Locations view

---
+++ src/views/LocationsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <header-container>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Localisations</h1>
+    </template>
+
+    <template #subtitle>
+      <h2 class="w-full my-4 text-base font-light">
+        Grands ou petits, chacun de nos bureaux est conçu pour inspirer
+        l'innovation, les grandes idées et la communauté.
+      </h2>
+    </template>
+  </header-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+export default defineComponent({
+  name: "LocationsView",
+  components: {
+    HeaderContainer,
+  },
+});
+</script>
