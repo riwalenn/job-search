4708dcd6f315e180c3eced1141f5724c25b55e12
RIWALENN BAS <riwalenn@gmail.com>
2022-09-29 21:25:47 +0200

    [MAJ] wiring up Search State to Vuex Store

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mt-5">
     <input
+      v-model="skillsSearchTerm"
       type="text"
       class="p-3 h-12 border border-solid border-brand-gray-1 shadow-gray rounded w-full text-base"
       placeholder="Computer programming, Finance degree"
@@ -9,9 +10,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+
+import { key } from "@/store";
+import { UPDATE_SKILLS_SEARCH_TERM } from "@/store/constants";
 
 export default defineComponent({
   name: "JobFiltersSidebarSkills",
+  setup() {
+    const store = useStore(key);
+    const skillsSearchTerm = computed({
+      get() {
+        return store.state.skillsSearchTerm;
+      },
+      set(value) {
+        store.commit(UPDATE_SKILLS_SEARCH_TERM, value);
+      },
+    });
+
+    return { skillsSearchTerm };
+  },
 });
 </script>
